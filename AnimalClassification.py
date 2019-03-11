36bf8c9fb9e04748d259b2f0484bf16fecbdb61c
Justin Huffman <jwhuffman@gmail.com>
2019-03-11 00:55:34 -0600

    Added train/test split.

--- AnimalClassification.py
+++ AnimalClassification.py
@@ -1,7 +1,11 @@
 import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 data = pd.read_pickle('data.pk')
 
-image = data.at[0,'image']
+X = np.array(data['image'])
+y = np.array(data['label'])
 
-print(image.shape)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2019)
+X_valid, X_test, y_valid, y_test = train_test_split(X_test, y_test, test_size=.66, random_state=2019)
