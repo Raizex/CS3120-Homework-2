28d862e909315571bd2c592b9e312fb55ef13069
Justin Huffman <jwhuffman@gmail.com>
2019-03-11 00:00:32 -0600

    Saved processed data to file.

--- AnimalClassification.py
+++ AnimalClassification.py
@@ -1,32 +1,7 @@
-#Credit to Tovio Roberts for writing most of the Image IO code, which can be found here:
-#https://github.com/clownfragment/CS3120_ML_suppl_mats
+import pandas as pd
 
-import cv2
-from os import listdir, getcwd
-from skimage.io import imread
-import matplotlib.pyplot as plt
+data = pd.read_pickle('data.pk')
 
-def plot_image(img,title):
-    #Plot image
-    fig, ax1 = plt.subplots(ncols=1, figsize=(18, 6), sharex=True,
-                            sharey=True)
-    ax1.imshow(img, cmap='gray')
-    ax1.set_title(title)
-    ax1.axis('off')
+image = data.at[0,'image']
 
-subfolder = 'data/KNN/animals/'
-classes = listdir(subfolder)
-classes.sort()
-
-cats_filenames = listdir(subfolder + classes[0])
-cats_filenames.sort()
-cats_filenames = [subfolder + classes[0] + '/' + \
-                  filename for filename in cats_filenames]
-cat0_rgb = imread(cats_filenames[0])
-plot_image(cat0_rgb, cats_filenames[0] + 'original')
-
-cat0_resize = cv2.resize(cat0_rgb, (32, 32),interpolation=cv2.INTER_CUBIC)
-plot_image(cat0_resize, cats_filenames[0] + 'resized')
-
-cat0_vect = cat0_resize.reshape(1,3072)
-print(cat0_vect.shape)
+print(image.shape)
---
+++ ImageProcessing.py
@@ -0,0 +1,49 @@
+#Credit to Tovio Roberts for writing most of the Image IO code, which can be found here:
+#https://github.com/clownfragment/CS3120_ML_suppl_mats
+
+import cv2
+import pandas as pd
+from os import listdir, getcwd
+from skimage.io import imread
+import matplotlib.pyplot as plt
+
+pd.set_option('display.max_colwidth', -1)
+
+def plot_image(img,title):
+    #Plot image
+    fig, ax1 = plt.subplots(ncols=1, figsize=(18, 6), sharex=True,
+                            sharey=True)
+    ax1.imshow(img, cmap='gray')
+    ax1.set_title(title)
+    ax1.axis('off')
+
+#Credit to kztd for generating dataframe from lists: https://stackoverflow.com/a/42723801    
+cols = ['filename','label','image']
+lst = []
+
+subfolder = 'data/KNN/animals/'
+classes = listdir(subfolder)
+classes.sort()
+
+for clas in classes:
+    class_filenames = listdir(subfolder + clas)
+    class_filenames.sort()
+    class_filenames = [subfolder + clas + '/' + \
+                       filename for filename in class_filenames]
+    
+    for animal_filename in class_filenames:
+        print('Processing: ' + animal_filename + '\n')
+        animal_rgb = imread(animal_filename)
+        animal_resize = cv2.resize(animal_rgb, (32, 32),interpolation=cv2.INTER_CUBIC)
+        
+        try:
+            animal_vect = animal_resize.reshape(1,3072)
+        except ValueError:
+            color_img = cv2.cvtColor(animal_resize, cv2.COLOR_GRAY2RGB)
+            plot_image(color_img,animal_filename + 'resize_and_convert')
+            animal_vect = color_img.reshape(1,3072)
+            
+        lst.append([animal_filename, clas[:-1], animal_vect])
+
+data = pd.DataFrame(lst, columns=cols)       
+data.to_pickle('data.pk')
